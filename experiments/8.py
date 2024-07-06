ea921c57b7cf34a81ed5d8b2e6fb6b8ffee973b6
gregorianrants <gregorian_rants@hotmail.com>
2024-07-06 21:09:26 +0100

    put experiments in there own folder

---
+++ experiments/8.py
@@ -0,0 +1,68 @@
+from BuildHat import BuildHat as Serial
+from Motor import Motor
+from Robot import Robot
+import time
+import signal
+import sys
+from sshkeyboard import listen_keyboard
+
+from robonet.src.Publisher import Publisher
+import zmq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+PI_IP = os.getenv("PI_IP")
+
+context = zmq.Context()
+publisher = Publisher(
+    context=context,
+    address=f"tcp://{PI_IP}",
+    publishers=[{"node": "robot", "topic": "left_motor"}],
+)
+
+
+serial = Serial()
+
+
+left_motor = Motor("C", serial, -1)
+right_motor = Motor("D", serial, 1)
+
+
+def signal_handler(sig, frame):
+    print("You pressed Ctrl+C!")
+    left_motor.run(0)
+    right_motor.run(0)
+    time.sleep(1)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+robot = Robot(left_motor, right_motor)
+
+
+def press(key):
+    print(f"'{key}' pressed")
+    if key == "k":
+        robot.forward()
+    if key == "m":
+        robot.back()
+    if key == "z":
+        robot.left()
+    if key == "x":
+        robot.right()
+    if key == "space":
+        robot.pause()
+
+
+def release(key):
+    print(f"'{key}' released")
+
+
+listen_keyboard(
+    on_press=press,
+    on_release=release,
+)
